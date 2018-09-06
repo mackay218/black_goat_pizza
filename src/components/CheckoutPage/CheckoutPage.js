809783fef3aab05d7f0a4b47e65c3cf47c57aee5
Abbey Janicek <abbey.janicek@gmail.com>
2018-09-06 14:23:53 -0500

    imported and set up class.

---
+++ src/components/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+class CheckoutPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+        }
+    }
+
+    // handleChange = (event) => 
+    render () {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
