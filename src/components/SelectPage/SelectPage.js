c310a68f1fa63e5f02ca50f83031e105b0c35a15
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-06 16:55:09 -0500

    remove pizza from redux store

--- src/components/SelectPage/SelectPage.js
+++ src/components/SelectPage/SelectPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PizzaList from './PizzaList/PizzaList.js';
+import { connect } from 'react-redux';
 
 class SelectPage extends Component {
     render() {
         return(
             <div>
+                <h1>TOTAL: {this.props.reduxStore.cart.order_total}</h1>
                 <PizzaList />
                 <button>Next</button>
             </div>
@@ -12,4 +14,7 @@ class SelectPage extends Component {
     }
 }
 
-export default SelectPage;
+const mapReduxStoreToProps = (reduxStore) => ({
+    reduxStore
+})
+export default connect(mapReduxStoreToProps)(SelectPage);
