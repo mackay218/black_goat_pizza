a6668afc46e14184b6c3cba50b0a15bb90cce7aa
Daniel MacKay <mackay2588@gmail.com>
2018-09-06 15:17:39 -0500

    added form for customer info

--- src/components/CustomerPage/CustomerPage.js
+++ src/components/CustomerPage/CustomerPage.js
@@ -8,9 +8,36 @@ class CustomerPage extends Component {
         return (
             <div className="customerContainer">
                 <h2>Customer Information</h2>
+                <p>{this.props.reduxState.cart.order_total}</p>
 
-
-
+                <div className="customerFormContainer">
+                    <form>
+                        <div className="formSection">
+                            <label htmlFor="#nameInput">Name</label>
+                            <input placeholder="Buzz Lightyear" />
+                        </div>
+                        <div className="formSection">
+                            <label htmlFor="#addressInput">Address</label>
+                            <input placeholder="Andy's Room"/>
+                        </div>
+                        <div className="formSection">
+                            <label htmlFor="#cityInput">City</label>
+                            <input placeholder="pixar city"/>
+                        </div>
+                        <div className="formSection">
+                            <label htmlFor="#zipInput">Zip Code</label>
+                            <input placeholder="55555" />
+                        </div>
+                        <div className="formSection">
+                            <label htmlFor="#pickBtn">Pickup</label>
+                            <input id="pickBtn" type="radio" value="pickup" name="pickOrDelv"/>
+                            <label htmlFor="#delvBtn">Delivery</label>    
+                            <input id="delvBtn" type="radio" value="delivery" name="pickOrDelv"/>
+                        
+                        </div>
+                    </form>
+                
+                </div>
 
             </div>
         );
